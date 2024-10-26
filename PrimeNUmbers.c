bbbc6fc5925ae4ebb54ad119e170777f86a71de0
preetham-22 <preethamreddy2226@gmail.com>
2024-10-26 19:02:21 +0530

    a

---
+++ PrimeNUmbers.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+int main() {
+    int firstNum,lastNum;
+    printf("Enter the First Number and Last Number: ");
+    scanf("%d %d",&firstNum,&lastNum);
+    for(i=firstNum; i<= lastNum; i++){
+        for(int j=0;j<Math.sqrt(i);j++){
+            if(i)
+        }
+    }
+}
+
